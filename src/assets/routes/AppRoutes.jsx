fe82328d8a36171c354f28d47adb8d6275658cc8
TousifsadequeChowdhury <tousifchowdhurybd@gmail.com>
2025-01-12 21:42:13 +0600

    add dynamic project

--- src/assets/routes/AppRoutes.jsx
+++ src/assets/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
@@ -8,30 +8,79 @@ import Registration from "../pages/Registration";
 import AddService from "../pages/AddService";
 import ManageService from "../pages/ManageService";
 import Service from "../components/Service";
-import ProviderDashboard from "../pages/ProviderDashboard ";
+// import ProviderDashboard from "../pages/ProviderDashboard";
 import ServiceDetails from "../pages/ServiceDetails";
 import Privateroutes from "./Privateroutes";
 import CustomerReviews from "../pages/CustomerReviews";
+
 function AppRoutes() {
+  const location = useLocation();
+
+  // Map of route paths to titles
+  const routeTitles = {
+    "/": "Home - FIXEDITUP",
+    "*": "Page Not Found - FIXEDITUP",
+    "/login": "Login - FIXEDITUP",
+    "/registration": "Register - FIXEDITUP",
+    "/services": "Services - FIXEDITUP",
+    "/customerevew": "Customer Reviews - FIXEDITUP",
+    "/addservice": "Add Service - FIXEDITUP",
+    "/manageservice": "Manage Services - FIXEDITUP",
+    "/servicetodo": "Dashboard - FIXEDITUP",
+  };
+
+  useEffect(() => {
+    // Get the title based on the current route
+    const currentPath = location.pathname;
+    const dynamicTitle = routeTitles[currentPath] || "FIXEDITUP";
+    document.title = dynamicTitle;
+  }, [location]);
+
   return (
     <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="*" element={<NotFound />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/registration" element={<Registration />} />
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registration" element={<Registration />} />
 
-    {/* Public Routes */}
-    <Route path="/services" element={<Service />} />
-    <Route path="/customerevew" element={<CustomerReviews />} />
+      {/* Public Routes */}
+      <Route path="/services" element={<Service />} />
+      <Route path="/customerevew" element={<CustomerReviews />} />
 
-    {/* Private Routes */}
-    <Route path="/addservice" element={<Privateroutes><AddService /></Privateroutes>} />
-      <Route path="/manageservice" element={<Privateroutes><ManageService /></Privateroutes>} />
-      <Route path="/servicetodo" element={<Privateroutes><ProviderDashboard /></Privateroutes>} />
-      <Route path="/services/:id" element={<Privateroutes><ServiceDetails /></Privateroutes>} />
-
-  </Routes>
-    
+      {/* Private Routes */}
+      <Route
+        path="/addservice"
+        element={
+          <Privateroutes>
+            <AddService />
+          </Privateroutes>
+        }
+      />
+      <Route
+        path="/manageservice"
+        element={
+          <Privateroutes>
+            <ManageService />
+          </Privateroutes>
+        }
+      />
+      <Route
+        path="/servicetodo"
+        element={
+          <Privateroutes>
+           <providerdeshbord/>
+          </Privateroutes>
+        }
+      />
+      <Route
+        path="/services/:id"
+        element={
+          <Privateroutes>
+            <ServiceDetails />
+          </Privateroutes>
+        }
+      />
+    </Routes>
   );
 }
 
